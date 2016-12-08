32a3460acfb3ed5de2b697b7b719f4cc5b09a592
Jose Pinto <zepinto@gmail.com>
2016-12-08 10:40:32 +0000

    Added Plans.kt DSL (work in progress).

--- src/main/kotlin/Plans.kt
+++ src/main/kotlin/Plans.kt
@@ -5,61 +5,87 @@ import pt.lsts.msg
 import java.time.Duration
 import java.util.*
 
-class Plan(val id : String) {
+class Plan(val id: String) {
 
-    var maneuvers = HashMap<String, Maneuver>()
-    var order = HashMap<Int, String>()
+    var mans = emptyArray<PlanManeuver>()
 
-    var loc:Geo = KnownGeos.APDL
+    var loc: Geo = KnownGeos.APDL.translatedBy(0.0, -200.0)
     var speed = 1.0
     var z = 0.0
     var z_units = "DEPTH"
     var count = 1
 
     fun <T : Maneuver> maneuver(id: String, m: Class<T>): T {
-        val man = msg(m) {
-            setValue("lat", loc.lat.asRadians())
-            setValue("lon", loc.lon.asRadians())
-            setValue("speed", speed)
-            setValue("speed_units", "METERS_PS")
-            setValue("z", z)
-            setValue("z_units", z_units)
+        val pman = msg(PlanManeuver::class.java) {
+            maneuverId = id
+            data = msg(m) {
+                setValue("lat", loc.lat.asRadians())
+                setValue("lon", loc.lon.asRadians())
+                setValue("speed", speed)
+                setValue("speed_units", "METERS_PS")
+                setValue("z", z)
+                setValue("z_units", z_units)
+            }
         }
-
-        order[order.size] = id
-        maneuvers[id] = man
-
-        return man
+        mans += pman;
+        return pman.data as T
     }
 
     fun goto(id: String = "${count++}") = maneuver(id, Goto::class.java)
 
-    fun skeeping(id: String = "${count++}", duration: Duration) : StationKeeping {
+    fun skeeping(id: String = "${count++}", radius: Double = 20.0, duration: Int = 0): StationKeeping {
         val man = maneuver(id, StationKeeping::class.java)
-        man.duration = duration.seconds.toInt()
+        man.duration = duration
+        man.radius = radius
         return man
     }
 
-    fun loiter(id: String = "${count++}", radius: Double, duration: Duration) : Loiter {
+    fun loiter(id: String = "${count++}", radius: Double = 20.0, duration: Int = 600): Loiter {
         val loiter = maneuver(id, Loiter::class.java)
-        loiter.duration = duration.seconds.toInt()
+        loiter.duration = duration
         loiter.type = Loiter.TYPE.CIRCULAR
         loiter.radius = radius
         return loiter
     }
 
-    fun yoyo(id:String = "${count++}", max_depth: Double, min_depth: Double) : YoYo {
+    fun yoyo(id: String = "${count++}", max_depth: Double = 20.0, min_depth: Double = 2.0): YoYo {
         val yoyo = maneuver(id, YoYo::class.java)
         yoyo.amplitude = (max_depth - min_depth)
         yoyo.z = (max_depth + min_depth) / 2.0
         return yoyo
     }
 
-    fun popup(id:String = "${count++}", duration: Duration, currPos: Boolean) : PopUp {
+    fun popup(id: String = "${count++}", duration: Int = 180, currPos: Boolean = true): PopUp {
         val popup = maneuver(id, PopUp::class.java)
-        popup.duration = duration.seconds.toInt()
+        popup.duration = duration
         popup.flags = if (currPos) PopUp.FLG_CURR_POS else 0;
         return popup
+    }
+
+    fun imc(): PlanSpecification {
+        return msg(PlanSpecification::class.java) {
+            planId = id;
+            startManId = mans[0].maneuverId
+            setManeuvers(mans.asList())
+            setTransitions(transitions().asList())
+        }
+    }
+
+    fun transitions(): Array<PlanTransition> {
+        var trans = emptyArray<PlanTransition>()
+        var previous: PlanManeuver? = null
+
+        for (m in mans) {
+            if (previous != null)
+                trans += msg(PlanTransition::class.java) {
+                    sourceMan = previous?.maneuverId
+                    destMan = m.maneuverId
+                    conditions = "maneuverDone"
+                }
+            previous = m
+        }
+
+        return trans;
     }
 }
 
@@ -68,10 +94,9 @@ fun main(args: Array<String>) {
     plan.speed = 1.2
     plan.z_units = "ALTITUDE"
     plan.z = 5.0
-
     plan.goto()
     plan.loc = plan.loc.translatedBy(100.0, 0.0)
-    plan.loiter(radius = 40.0, duration = Duration.ofMinutes(3))
-    println(plan.maneuvers)
+    plan.loiter(duration = 3*60)
+    println(plan.imc().asXml(false))
 }
 
